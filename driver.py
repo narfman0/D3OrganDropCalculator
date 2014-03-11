e5c516436705222fe66f05818d640b3da25229b3
Jon Robison <jrobison@alionscience.com>
2014-03-11 17:23:55 -0400

    -Fix some formatting

--- driver.py
+++ driver.py
@@ -1,24 +1,25 @@
 #!/bin/python
 from calculator import calculate
+from math import pow
 
 RUNS=10
 
 def prettify(number):
-  return '{0:.2f}'.format(number)
+  return '{:5.2f}'.format(number)
 
 totals=[]
 for torment in range(1,7):
   total = []
   for runs in range(0,RUNS):
-    total.append(calculate(torment,runs+1))
+    total.append(pow(calculate(torment,runs+1), 3))
   totals.append(total)
 
 runsString = ''
 for run in range(1,RUNS+1):
-  runsString += str(run) + '     '
+  runsString += '{:6.0f}'.format(run)
 print('\tTorment\\Runs ' + runsString)
 for torment in range(0,len(totals)):
-  row='\t     ' + str(torment+1) + ":  "
+  row='\t     ' + str(torment+1) + ":       "
   for runs in range(0, len(totals[torment])):
     row += prettify(totals[torment][runs]*100) + ' '
   print(row)
